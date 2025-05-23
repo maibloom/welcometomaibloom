cd23264500a965d3bb85749f5fd1fb6e7f726e6b
Amirali Dashti <devoft1@gmail.com>
2025-05-23 17:16:20 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 sudo chmod +x *
-sudo cp welcometomaibloom.py /usr/local/bin/welcometomaibloom
-sudo cp logo.png /usr/local/bin/welcometomaibloom
+sudo cp welcometomaibloom.py /usr/bin/welcometomaibloom
+sudo cp logo.png /usr/bin/welcometomaibloom
 
 sudo cp welcometomaibloom.desktop /usr/share/applications/
 sudo update-desktop-database /usr/share/applications/
