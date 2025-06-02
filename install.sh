92a17d937aabc95df3e66a4f7ec56247541df64d
Amirali Dashti <devoft1@gmail.com>
2025-06-02 22:50:27 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo cp welcometomaibloom.py /usr/local/bin/welcometomaibloom
 sudo cp logo.png /usr/local/bin/welcometomaibloom
 
 sudo cp welcometomaibloom.desktop /usr/share/applications/
-sudo cp welcometomaibloom.desktop $(xdg-user-dir DESKTOP)/
+sudo cp welcometomaibloom.desktop $(xdg-user-dir)/Desktop
 sudo update-desktop-database /usr/share/applications/
 
 echo "Installation completed!"
