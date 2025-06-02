bb42114fddca7819d706053070a4b125b4d9e355
Amirali Dashti <devoft1@gmail.com>
2025-06-02 22:47:13 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
-USER_HOME=$(eval echo "~${SUDO_USER}")
-sudo chmod +x welcometomaibloom.py
-sudo mkdir -p /usr/local/bin/welcometomaibloom
-sudo cp welcometomaibloom.py /usr/local/bin/welcometomaibloom/
-sudo cp logo.png /usr/local/bin/welcometomaibloom/
+sudo chmod +x *
+sudo mkdir /usr/local/bin/welcometomaibloom
+sudo cp welcometomaibloom.py /usr/local/bin/welcometomaibloom
+sudo cp logo.png /usr/local/bin/welcometomaibloom
+
 sudo cp welcometomaibloom.desktop /usr/share/applications/
-sudo cp welcometomaibloom.desktop "$USER_HOME/Desktop/"
-sudo -u "$SUDO_USER" chmod +x "$USER_HOME/Desktop/welcometomaibloom.desktop"
+sudo cp welcometomaibloom.desktop $(xdg-user-dir DESKTOP)
 sudo update-desktop-database /usr/share/applications/
+
 echo "Installation completed!"
